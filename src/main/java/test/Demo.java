1ff984bf8be0e0f4e2f998f903c2fddb840dfbe9
krasnovss91 <krasnovstanislav91@gmail.com>
2021-11-05 17:55:52 +0400

    addLombok

--- src/main/java/test/Demo.java
+++ src/main/java/test/Demo.java
@@ -1,29 +1,16 @@
 package test;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
 public class Demo {
 
     public int x;
     public int y;
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public Demo() {
-
-    }
 
     public int sumOfSquare(int x, int y) {
         return (x * x) + (y * y);
